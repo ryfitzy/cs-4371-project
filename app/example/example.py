992870ce63827626ac409ec9de85a15781306340
Colin Crippen <125526405+colincrippen@users.noreply.github.com>
2025-04-05 02:05:11 -0500

    Added basic Ollama support

--- app/example/example.py
+++ app/example/example.py
@@ -1,6 +1,8 @@
+import ollama
 from flask import render_template, request
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, TextAreaField
+from ollama import ChatResponse
+from wtforms import SelectField, SubmitField, TextAreaField
 
 from . import bp
 
@@ -8,6 +10,10 @@ from . import bp
 class DemoForm(FlaskForm):
     prompt = TextAreaField("Enter prompt here")
     submit = SubmitField("Submit")
+    models = SelectField(
+        "Select model:",
+        choices=[("mistral", "Mistral"), ("gemma3", "Gemma 3"), ("llama3.2", "Llama 3.2")],
+    )
 
 
 @bp.route("/", methods=["GET", "POST"])
@@ -15,5 +21,14 @@ def demo():
     form = DemoForm(request.form)
     if request.method == "GET":
         return render_template("demo.html", form=form)
-
-    return render_template("demo.html", form=form)
+    else:
+        response: ChatResponse = ollama.chat(
+            model=form.models.data,
+            messages=[
+                {
+                    "role": "user",
+                    "content": form.prompt.data,
+                },
+            ],
+        )
+        return render_template("demo.html", form=form, response=response.message.content)
